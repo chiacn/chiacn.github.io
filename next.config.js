9278c50147d473f973a76f2d1a556f6ec431e88e
chiacn <chiacn@naver.com>
2023-03-30 10:38:08 +0900

    배포 설정 변경2

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   // Optionally, add any other Next.js config below
   reactStrictMode: true,
-  basePath: ''
+  basePath: ""
 }
 
 module.exports = withPlugins(
