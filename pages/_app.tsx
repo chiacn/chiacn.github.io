e81b205a06ff0f010f339d6e752ae8f84d602003
chiacn <chiacn@naver.com>
2023-05-21 23:06:06 +0900

    PrimeJS 테마 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@/styles/prismjs/prism.css'
+import '@/styles/prismjs/prism.js'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
