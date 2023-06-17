1e765acaa139d509d67e52de38d713587e0884e2
chiacn <chiacn@naver.com>
2023-06-17 17:46:43 +0900

    (PrimeJS) 코드 블럭 하이라이트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import '@/styles/prismjs/prism.css'
-import '@/styles/prismjs/prism.js'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "@/styles/prismjs/prism.css";
+import "@/styles/prismjs/prism.js";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
